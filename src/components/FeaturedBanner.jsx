c28c51928f408639e85a1813dc3839a49ebb743c
Dolly Aggarwal <dollyaggarwal1712@gmail.com>
2026-03-15 18:23:39 +0530

    removed unused libraries

--- src/components/FeaturedBanner.jsx
+++ src/components/FeaturedBanner.jsx
@@ -1,5 +1,4 @@
 import { useNavigate } from "react-router-dom";
-import { useEffect, useRef } from "react";
 import { motion } from "framer-motion";
 
 export default function FeaturedBanner({ artists }) {
